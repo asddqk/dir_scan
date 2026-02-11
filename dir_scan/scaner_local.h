f7acc34b96ec78ef7cab1d52895c5108390172e5
asddqk <asddqk@icloud.com>
2026-02-11 22:00:29 +0500

    adding kw to rwtab[]

--- dir_scan/scaner_local.h
+++ dir_scan/scaner_local.h
@@ -15,20 +15,21 @@ struct find_string {
 
 
 struct find_string rwtab[] = {
-    {"abort", kwAbort},
-    {"begin", kwBegin},
-    {"case",  kwCase},
-    {"end",   kwEnd},
-    {"float", kwFloat},
-    {"goto",  kwGoto},
-    {"int",   kwInt},
-    {"loop",  kwLoop},
-    {"read",  kwRead},
-    {"skip",  kwSkip},
-    {"space", kwSpace},
-    {"tab",   kwTab},
-    {"var",   kwVar},
-    {"write", kwWrite}
+    {"vector", kwVECTOR},
+    {"of", kwOF},
+    {"integer", kwINTEGER},
+    {"real", kwREAL},
+    {"goto", kwGOTO},
+    {"read", kwREAD},
+    {"write", kwWRITE},
+    {"skip", kwSKIP},
+    {"space", kwSPACE},
+    {"tab", kwTAB},
+    {"if", kwIF},
+    {"then", kwTHEN},
+    {"else", kwELSE},
+    {"while", kwWHILE},
+    {"do", kwDO}
 };
 
 int rw_size = sizeof(rwtab) / sizeof(find_string);
